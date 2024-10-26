f80cab769de3036bf05dba4f484f74e0efcc81d8
dwanibantas <totokww@gmail.com>
2024-10-26 20:37:15 +0700

    Add files via upload

---
+++ data/IbukotaKabupatenKota_3.js
@@ -0,0 +1,12 @@
+var json_IbukotaKabupatenKota_3 = {
+"type": "FeatureCollection",
+"name": "IbukotaKabupatenKota_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMA": "Sleman", "WIKI": "https:\/\/id.wikipedia.org\/wiki\/Sleman,_Sleman", "FOTO": "https:\/\/www.suaramerdeka.com\/storage\/images\/2018\/05\/14\/rumah-dinas-bupati-sleman-5af953c70ef2d.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.35500512883128, -7.715329572764386 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Wates", "WIKI": "https:\/\/id.wikipedia.org\/wiki\/Wates,_Kulon_Progo", "FOTO": "http:\/\/1.bp.blogspot.com\/-UD3LsCryr0o\/UqaNEJoZJrI\/AAAAAAAABUo\/kyH17Rl_LK4\/s1600\/43967321.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.159545252181175, -7.856236736013338 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Yogyakarta", "WIKI": "https:\/\/id.wikipedia.org\/wiki\/Kota_Yogyakarta", "FOTO": "https:\/\/www.yogyes.com\/en\/yogyakarta-travel-guide\/12-free-attractions-in-jogja\/10.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.373029341872467, -7.803800858220321 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Bantul", "WIKI": "https:\/\/id.wikipedia.org\/wiki\/Bantul,_Bantul", "FOTO": "http:\/\/jogjaupdate.com\/wp-content\/uploads\/2017\/02\/Bantul.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.327176254799255, -7.884824585758197 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Wonosari", "WIKI": "https:\/\/id.wikipedia.org\/wiki\/Wonosari,_Gunung_Kidul", "FOTO": "https:\/\/www.yogyes.com\/en\/yogyakarta-hotel\/by-location\/wonosari\/1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.596680179185469, -7.958111386589406 ] } }
+]
+}
